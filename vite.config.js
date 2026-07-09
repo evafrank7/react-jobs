ca79b45e9551e98578ec4918973a6828e6ffda3b
Eva Frankovic <emfrankovic7@gmail.com>
2026-07-08 20:03:38 -0400

    Connect frontend to Render jobs API

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api' : {
-        target: 'http://localhost:8000',
+        target: 'https://react-jobs-gt2z.onrender.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
